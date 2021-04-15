bedb84e50891d54c4879c22d24c4e11cb43aadd5
Fernando Costella <nandocostella@gmail.com>
2021-04-15 13:53:11 -0300

    alterado tamanho padrao do navegador

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -4,4 +4,10 @@ Before do
   @signup_page = SignupPage.new
   @dash_page = DashPage.new
   @equipos_page = EquiposPage.new
+
+  #comando para maximizar a tela do navegador
+  #page.driver.browser.manage.window.maximize
+
+  #determina um tamanho específico para a janela do navegador
+  page.current_window.resize_to(1140, 900)
 end
