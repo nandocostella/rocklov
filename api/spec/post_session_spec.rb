d79405314c33ee67ebf5f23d2f31519f0c98aca5
Fernando Costella <nandocostella@gmail.com>
2021-04-28 17:43:39 -0300

    alterando para padrão OO no projeto da API

--- api/spec/post_session_spec.rb
+++ api/spec/post_session_spec.rb
@@ -1,18 +1,10 @@
-require "httparty"
+require_relative "routes/sessions"
 
 describe "POST /session" do
   context "login com sucesso" do
     # o :all faz com que o before execute apenas uma vez o before
     before (:all) do
-      payload = { email: "teste@teste.com", password: "123456" }
-
-      @result = HTTParty.post(
-        "http://rocklov-api:3333/sessions",
-        body: payload.to_json,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      )
+      @result = Sessions.new.login("teste@teste.com", "123456")
     end
 
     it "valida status code" do
@@ -23,4 +15,20 @@ describe "POST /session" do
       expect(@result.parsed_response["_id"].length).to eql 24
     end
   end
+
+  context "senha invalida" do
+    # o :all faz com que o before execute apenas uma vez o before
+    before (:all) do
+      @result = Sessions.new.login("teste@teste.com", "aaaaaa")
+    end
+
+    it "valida status code" do
+      expect(@result.code).to eql 401
+    end
+
+    it "valida id do usuário" do
+      expect(@result.parsed_response["error"]).to eql "Unauthorized"
+    end
+  end
+
 end
