795526e51ff9749c7c1d1720d2d6261a844d3185
Fernando Costella <nandocostella@gmail.com>
2021-04-30 13:37:20 -0300

    criado base api para especificar a conexao

--- api/spec/routes/sessions.rb
+++ api/spec/routes/sessions.rb
@@ -1,16 +1,13 @@
-require "httparty"
+require_relative "base_api"
 
-class Sessions
-  include HTTParty
-  base_uri "http://rocklov-api:3333"
-
-  def login (payload)    
+class Sessions < BaseApi
+  def login(payload)
     return self.class.post(
-      "/sessions",
-      body: payload.to_json,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    )
+             "/sessions",
+             body: payload.to_json,
+             headers: {
+               "Content-Type": "application/json",
+             },
+           )
   end
 end
