a963ea4fdf4a12f07debe2c773176110b026adce
Fernando <nandocostella@gmail.com>
2021-05-01 14:10:07 -0300

    implementado cenario de cadastro duplicado

--- api/spec/post_signup_spec.rb
+++ api/spec/post_signup_spec.rb
@@ -18,4 +18,29 @@ describe "POST /signup" do
       expect(@result.parsed_response["_id"].length).to eql 24
     end
   end
+
+  context "usuario ja existe" do
+    before(:all) do
+      # dado que eu tenha um novo usuario
+      payload = { name: "Teste2", email: "teste2@teste.com", password: "123456" }
+      MongoDB.new.remove_users(payload[:email])
+
+      # e o email desse usuario ja esta cadastrado no sistema
+      Signup.new.create(payload)
+
+      # quando faço uma requisição para a rota /signup
+      @result = Signup.new.create(payload)
+    end
+
+    # entao deve retornar 409
+    it "deve retornar 409" do
+      expect(@result.code).to eql 409
+    end
+
+    it "deve retornar mensagem" do
+      expect(@result.parsed_response["error"]).to eql "Email already exists :("
+    end
+
+  end
+
 end
