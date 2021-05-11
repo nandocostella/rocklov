579df84d9df979e5690306183d9e7683c6ca4653
Fernando Costella <nandocostella@gmail.com>
2021-05-11 13:37:50 -0300

    criacao das sementes com rake e iniciada feature de remocao de anuncios

---
+++ web/features/specs/remover-anuncios.feature
@@ -0,0 +1,29 @@
+#language: pt
+
+Funcionalidade: Remover anúncios
+  Sendo um anunciante que possui um equipamento indesejado
+  Quero poder remover esse anúncio
+  Para que eu possa o meu dashboard atualizado
+
+  Cenario: Remover anúncio
+
+    Dado que eu tenho um anúncio indesejado:
+      | thumb     | telecaster.jpg |
+      | nome      | Telecaster     |
+      | categoria | Cordas         |
+      | preco     | 50             |
+    Quando solicito a exclusão desse item
+      E confirmo a exclusão
+    Entao não devo ver esse item no meu dashboard
+
+
+  Cenario: Desistir da exclusão
+
+    Dado que eu tenho um anúncio indesejado:
+      | thumb     | conga.jpg |
+      | nome      | Conga     |
+      | categoria | Outros         |
+      | preco     | 100             |
+    Quando solicito a exclusão desse item
+      Mas não confirmo a solicitação
+    Entao devo ver esse item no meu dashboard
--- web/features/support/libs/mongo.rb
+++ web/features/support/libs/mongo.rb
@@ -3,23 +3,33 @@ require "mongo"
 Mongo::Logger.logger = Logger.new("./logs/mongo.log")
 
 class MongoDB
+  attr_accessor :client, :users, :equipos
+
+  def initialize
+    @client = Mongo::Client.new("mongodb://rocklov-db:27017/rocklov")
+    @users = client[:users]
+    @equipos = client[:equipos]
+  end
+
+  def drop_danger
+    client.database.drop
+  end
+
+  def insert_users(docs)
+    @users.insert_many(docs)
+  end
+
   def remove_users(email)
-    client = Mongo::Client.new("mongodb://rocklov-db:27017/rocklov")
-    users = client[:users]
     users.delete_many({ email: email })
   end
 
   def get_user(email)
-    client = Mongo::Client.new("mongodb://rocklov-db:27017/rocklov")
-    users = client[:users]
     user = users.find({ email: email }).first
     return user[:_id]
   end
 
   def remove_equipo(name, email)
     user_id = get_user(email)
-    client = Mongo::Client.new("mongodb://rocklov-db:27017/rocklov")
-    equipos = client[:equipos]
     equipos.delete_many({ name: name, user: user_id })
   end
 end
